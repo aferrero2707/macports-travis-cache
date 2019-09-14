d00fc6edf329e0bc5050393c34409236f0ddf595
aferrero2707 <aferrero1975@gmail.com>
2019-09-14 18:10:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 bash ./travis_wait.sh &
 yes | sudo /opt/local/bin/port install gtkmm +quartz
 yes | sudo /opt/local/bin/port install gtkmm3 +quartz
-yes | sudo /opt/local/bin/port install fftw-3-single libiptcdata lensfun lcms2 expat tiff libjpeg-turbo libpng zlib gtk-osx-application-gtk3            
+yes | sudo /opt/local/bin/port install fftw-3-single libiptcdata lensfun lcms2 expat tiff jpeg libpng zlib gtk-osx-application-gtk3            
