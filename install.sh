89807209286c4080b4d3832815fe3da24baff631
aferrero2707 <aferrero1975@gmail.com>
2019-09-14 14:37:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@
 curl -LO https://raw.githubusercontent.com/GiovanniBussi/macports-ci/master/macports-ci
 source ./macports-ci install
 bash ./travis_wait.sh &
-yes | sudo /opt/local/bin/port install gtk2 +quartz
-yes | sudo /opt/local/bin/port install gtk3 +quartz
-#yes | sudo /opt/local/bin/port install gtkmm +quartz
-#yes | sudo /opt/local/bin/port install gtkmm3 +quartz
-#yes | sudo /opt/local/bin/port install fftw-3-single libiptcdata lensfun lcms2 expat libtiff libjpeg libpng zlib gtk-osx-application-gtk3            
+yes | sudo /opt/local/bin/port install gtkmm +quartz
+yes | sudo /opt/local/bin/port install gtkmm3 +quartz
+yes | sudo /opt/local/bin/port install fftw-3-single libiptcdata lensfun lcms2 expat libtiff libjpeg libpng zlib gtk-osx-application-gtk3            
